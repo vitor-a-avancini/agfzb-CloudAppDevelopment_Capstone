86c1a3a076f935c076e6102544d9f306d18c9125
Vitor Avancini <vtravancini@gmail.com>
2022-06-20 15:11:45 +0000

    with migrations

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,48 +4,31 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
 class CarMake(models.Model):
-    name = models.CharField(null=False, max_length=30)
-    description = models.CharField(max_length=280)
+    name = models.CharField(null= False, max_length=30, default='Audi')
+    description = models.CharField(null= False, max_length=300, default='Audi cars are aight')
 
     def __str__(self):
-        return self.name
+        return 'Name:' + self.name + ',' + \
+            'Description:' + self.description
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+
 class CarModel(models.Model):
-    #constant for c_type choices
-    TYPES = (
-            ("SEDAN", "Sedan"), ("SUV", "SUV"), ("WAGON", "Wagon"), ("LIMOUSINE", "Limousine"), ("BATMOBILE", "Batmobile")
-        )
-
-    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    name = models.CharField(null=False, max_length=30)
-    c_type = models.CharField(max_length=30, choices=TYPES)
-    dealer_id = models.IntegerField()
-    year = models.DateField()
+    SEDAN = 'sedan'
+    SUV = 'suv'
+    WAGON = 'wagon'
+    OTHERS = 'others'
+    CAR_CHOICES = [(SEDAN, "Sedan"), (SUV, 'SUV'), (WAGON, 'Wagon'), (OTHERS, 'Others')]
+    carmake = models.ForeignKey(CarMake, null= True, on_delete=models.CASCADE)
+    name = models.CharField(null= False, max_length=30, default='Audi X8')
+    dealerid = models.IntegerField(null=True)
+    cartype = models.CharField(null= False, max_length=20, choices= CAR_CHOICES, default=SEDAN)
+    year = models.DateField(null= True)
 
     def __str__(self):
-        return "Name: " + self.name + \
-                " Make Name: "+ self.make.name + \
-                " Type: " + self.c_type + \
-                " Dealer ID: " + str(self.dealer_id)+ \
-                " Year: " + str(self.year)
-                
-
-
+        return 'Name ' + self.name
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+
 class CarDealer:
 
     def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
@@ -67,24 +50,24 @@ class CarDealer:
         self.st = st
         # Dealer zip
         self.zip = zip
+        self.idx = 0
 
     def __str__(self):
         return "Dealer name: " + self.full_name
 
+
 # <HINT> Create a plain Python class `DealerReview` to hold review data
 class DealerReview:
-    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year,sentiment, id):
-        self.dealership=dealership
-        self.name=name
-        self.purchase=purchase
-        self.review=review
-        self.purchase_date=purchase_date
-        self.car_make=car_make
-        self.car_model=car_model
-        self.car_year=car_year
-        self.sentiment=sentiment #Watson NLU service
-        self.id=id
+    def __init__(self, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year, sentiment):
+        self.name = name
+        self.dealership = dealership
+        self.review = review
+        self.purchase = purchase
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.sentiment = sentiment
 
     def __str__(self):
-        return "Review: " + self.review +\
-                " Sentiment: " + self.sentiment
+        return "Review: " + self.review
